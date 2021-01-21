81c8443b8383bf685b3a0d19ab64312c6cd12fb8
badimhmm <badimhmm>
2021-01-21 18:08:33 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+	// client connect akan masuk sini
+	console.log(`Socket.io client connected`);
+	socket.emit('init', { message: 'Ini dari server' })
+})
+
+server.listen(3000, () => {
+	console.log(`Whackme listening on port ` + 3000);
+})
