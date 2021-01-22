e69c174bdab5cc29ef0eb37bb2b191d13693b674
agungmrizqon <agungrizqon18@gmail.com>
2021-01-22 10:47:51 +0700

    update server

--- app.js
+++ app.js
@@ -4,39 +4,68 @@ const io = require('socket.io')(server);
 
 // setInterval(function())
 
+let insertUser = []
+
 
 io.on('connection', function(socket) {
-	function randomX () {
-		return Math.floor(Math.random()*7)
-	}
-	function randomY () {
-		return Math.floor(Math.random()*7)
-	}
-	
-	let position = {
-		x: randomX(),
-		y: randomY()
-	}
-	
-	console.log(`Socket.io client connected`);
-	io.emit('position', position)
-	
+		socket.on('startGame', function () {
+			function randomX () {
+				return Math.floor(Math.random()*7)
+			}
+			function randomY () {
+				return Math.floor(Math.random()*7)
+			}
+			
+			let position = {
+				x: randomX(),
+				y: randomY()
+			}
+			
+			console.log(`Socket.io client connected`);
+			io.emit('position', position)
 
-	socket.on('changePosition', function () {
-		function randomX () {
-			return Math.floor(Math.random()*7)
-		}
-		function randomY () {
-			return Math.floor(Math.random()*7)
-		}
+		})
 		
-		let position = {
-			x: randomX(),
-			y: randomY()
-		}
-
-		io.emit('position', position)
+		socket.on('changePosition', function () {
+	
+				function randomX () {
+					return Math.floor(Math.random()*7)
+				}
+				function randomY () {
+					return Math.floor(Math.random()*7)
+				}
+				let position = {
+					x: randomX(),
+					y: randomY()
+				}
+				io.emit('position', position)
+	
+		})
+	
+			
+		socket.on('insertUser', function (payload) {
+		
+		if(insertUser.length === 0) {
+			let obj = {
+				player: "playerOne",
+				name: payload,
+				sum: insertUser.length + 1
+			}	
+			insertUser.push(obj)
+			socket.emit('insertUserToClient', insertUser)
+		} else {
+			let obj = {
+				player: "playerTwo",
+				name: payload,
+				sum: insertUser.length + 1
+			}
+			insertUser.push(obj)
+			socket.emit('insertUserToClient', insertUser)
+			socket.broadcast.emit('updateSum', insertUser)
+		}		
 	})
+
+	
 })
 
 server.listen(3000, () => {
